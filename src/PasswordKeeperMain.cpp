858b78a68b95e0b822a0febd8777c8077b919fde
Dandi91 <dandi.vlg@gmail.com>
2014-06-18 19:52:37 -0700

    Linux fixes

--- src/PasswordKeeperMain.cpp
+++ src/PasswordKeeperMain.cpp
@@ -518,13 +518,8 @@ void PasswordKeeperFrame::OnMouseEvent(wxMouseEvent& event)
 void PasswordKeeperFrame::OnTabsRightUp(wxMouseEvent& event)
 {
   int selection = tbTabs->HitTest(event.GetPosition());
-<<<<<<< Updated upstream
-  if (selection != -1)
-    tbTabs->SetSelection();
-=======
   if (selection > -1)
     tbTabs->SetSelection(selection);
->>>>>>> Stashed changes
   UpdateMenus();
   tbTabs->PopupMenu(&puTabMenu);
 }
