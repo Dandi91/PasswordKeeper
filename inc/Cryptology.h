3459fb97822c868039893c1882912ddef134d820
Dandi91 <dandi.vlg@gmail.com>
2014-06-19 04:02:47 -0700

    Cryptopp library RSA implementation Single crypto-functions are no longer class members

---
+++ inc/Cryptology.h
@@ -0,0 +1,37 @@
+#ifndef CRYPTOLOGY_H
+#define CRYPTOLOGY_H
+
+// Cryptology.h provides interfaces for cryptopp library algorythms that are used by application
+
+#include <rsa.h>
+
+#include <wx/string.h>
+#include <wx/buffer.h>
+
+// CRSAEncryption provides CryptoPP::RSA::PublicKey wrapping for more convinient use
+class CRSAEncryption
+{
+private:
+  bool isValid;
+  CryptoPP::RSA::PublicKey rsaPublic;
+
+public:
+  CRSAEncryption() { isValid = false; };
+
+  // RSA functions
+  bool LoadPublicRSAKey(const wxString& fileName);
+  void RSAEncrypt(wxMemoryBuffer& dest, const wxMemoryBuffer& source);
+  bool RSAVerify(const wxMemoryBuffer& source, const wxMemoryBuffer& signature);
+};
+
+// Static functions for AES and digests
+
+// AES two-way function
+void AESEncrypt(wxMemoryBuffer& dest, const wxMemoryBuffer& source, const size_t length, const wxMemoryBuffer& key, const wxMemoryBuffer& iv);
+void XORDigestIV(wxMemoryBuffer& buffer);  // Makes 128-bit IV from 256-bit digest
+
+// Didgests
+void SHADigest(const wxString& plane, wxMemoryBuffer& digest);
+void CRCSum(const wxMemoryBuffer& buffer, unsigned long* lvalue);
+
+#endif // CRYPTOLOGY_H
